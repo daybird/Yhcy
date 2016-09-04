2decfec9930e0fe9359b59053ebc621806d72a43
daybird <917856677@qq.com>
2016-09-04 11:26:32 +0800

    zc

---
+++ app/src/main/java/com/s/yhcy/activity/MyAppCompatActivity.java
@@ -0,0 +1,17 @@
+package com.s.yhcy.activity;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * AppCompatActivity 子类 用于去除actionBar
+ * Created by PawN on 2016/9/4.
+ */
+public class MyAppCompatActivity extends AppCompatActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+//        ActionBar supportActionBar = getSupportActionBar();
+//        supportActionBar.hide();
+    }
+}
