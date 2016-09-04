3fe4db3637fde3d90f8ee02181d49ac5e2a693bc
daybird <917856677@qq.com>
2016-09-04 23:11:17 +0800

    zc

--- app/src/main/java/com/s/yhcy/activity/MainActivity.java
+++ app/src/main/java/com/s/yhcy/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.s.yhcy.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,12 +11,14 @@ import com.s.yhcy.R;
 
 public class MainActivity extends MyAppCompatActivity {
 
+    private static final int FILE_SELECT_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button gsxdBt = (Button) this.findViewById(R.id.gsxdBt);
-        Button excelInputBt =(Button)this.findViewById(R.id.excelInputBt);
+        Button excelInputBt = (Button) this.findViewById(R.id.excelInputBt);
         gsxdBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,7 +31,7 @@ public class MainActivity extends MyAppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("*/*");
                 intent.addCategory(Intent.CATEGORY_OPENABLE);
-                startActivityForResult(intent,1);
+                startActivityForResult(intent, FILE_SELECT_CODE);
             }
         });
     }
@@ -38,18 +41,25 @@ public class MainActivity extends MyAppCompatActivity {
      */
     private void showGsxdActivity() {
         Intent intent = new Intent();
-        intent.setClass(this,GsxdActivity.class);
+        intent.setClass(this, GsxdActivity.class);
         this.startActivity(intent);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data==null){
-            Toast.makeText(this,"没有接收到数据！",Toast.LENGTH_SHORT).show();
+        if (data == null) {
+            Toast.makeText(this, R.string.noData, Toast.LENGTH_SHORT).show();
         }
-        Bundle bundle = data.getExtras();
-        String result = bundle.getParcelable("data");
-        Toast.makeText(this,result,Toast.LENGTH_SHORT).show();
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    Toast.makeText(this, uri.getPath(), Toast.LENGTH_SHORT).show();
+                } else if (resultCode == RESULT_CANCELED) {
+                    Toast.makeText(this, R.string.cancelChoice, Toast.LENGTH_SHORT).show();
+                }
+        }
+
 
     }
 }
