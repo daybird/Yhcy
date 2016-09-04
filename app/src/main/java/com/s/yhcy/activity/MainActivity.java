8812eaaeeda2152104517299de70510bdb288a73
daybird <917856677@qq.com>
2016-09-04 18:43:06 +0800

    zc

--- app/src/main/java/com/s/yhcy/activity/MainActivity.java
+++ app/src/main/java/com/s/yhcy/activity/MainActivity.java
@@ -1,11 +1,10 @@
 package com.s.yhcy.activity;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.s.yhcy.R;
 
@@ -16,10 +15,20 @@ public class MainActivity extends MyAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button gsxdBt = (Button) this.findViewById(R.id.gsxdBt);
+        Button excelInputBt =(Button)this.findViewById(R.id.excelInputBt);
         gsxdBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showGsxdActivity();
+            }
+        });
+        excelInputBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("*/*");
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent,1);
             }
         });
     }
@@ -31,5 +40,11 @@ public class MainActivity extends MyAppCompatActivity {
         Intent intent = new Intent();
         intent.setClass(this,GsxdActivity.class);
         this.startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        String result = data.getExtras().getString("result");
+        Toast.makeText(this,result,Toast.LENGTH_SHORT).show();
     }
 }
